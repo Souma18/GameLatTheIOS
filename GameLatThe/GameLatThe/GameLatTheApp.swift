1ebdafe27321564b967ba056cbf48e5271b94b3e
Lê Lương Thuỷ Nguyên <leluongthuynguyen@192.168.1.21.non-exists.ptr.local>
2025-10-12 23:11:01 +0700

    them cau truc folder

---
+++ GameLatThe/GameLatThe/GameLatTheApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameLatTheApp.swift
+//  GameLatThe
+//
+//  Created by Lê Lương Thuỷ Nguyên on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GameLatTheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
